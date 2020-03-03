f5983ab2696f50c5cfa59f17282fcf38e21a1fd9
Cosmin-Constantin Oprea <cosmin.constantin.oprea@gmail.com>
2020-03-03 16:44:40 +0100

    Add logging

--- java/springboot/src/main/java/com/redhat/developer/demos/preference/PreferencesApplication.java
+++ java/springboot/src/main/java/com/redhat/developer/demos/preference/PreferencesApplication.java
@@ -22,16 +22,16 @@ public class PreferencesApplication extends WebMvcConfigurerAdapter {
 		return restTemplateBuilder.build();
 	}
 
-	@Bean
-	public FilterRegistrationBean getPeticionFilter() {
-
-		FilterRegistrationBean registration = new FilterRegistrationBean();
-		registration.setFilter(new RequestFilter());
-		registration.addUrlPatterns("/*");
-		registration.setName("requestFilter");
-
-		return registration;
-	}
+//	@Bean
+//	public FilterRegistrationBean getPeticionFilter() {
+//
+//		FilterRegistrationBean registration = new FilterRegistrationBean();
+//		registration.setFilter(new RequestFilter());
+//		registration.addUrlPatterns("/*");
+//		registration.setName("requestFilter");
+//
+//		return registration;
+//	}
 
 //	@Bean
 //	public io.opentracing.Tracer tracer() {
